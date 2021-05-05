b40e2b8d6c1da9eb4461694e72ddee537cb0a3aa
RaghadALBoriny <>
2021-05-05 11:37:55 +0000

    to add a js

---
+++ app.js
@@ -0,0 +1,10 @@
+alert("Hello have a great time here");
+
+var favouriteInstrument=prompt('What do you like more Guitar or Ney?')
+
+if(favouriteInstrument=='Guitar')
+{document.write('<iframe width="420" height="345" src="https://www.youtube.com/embed/_ZaRQmLuChc?autoplay"></iframe>')}
+
+else if(favouriteInstrument=='Ney')
+{document.write('<iframe width="420" height="345" src="https://www.youtube.com/embed/JFjoVCIdEgw autoplay"></iframe>')}
+

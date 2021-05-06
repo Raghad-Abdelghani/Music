ceed0a6bb8216d8b2a936c2423fc8249917deebc
RaghadALBoriny <>
2021-05-06 12:02:27 +0000

    added codes

--- app.js
+++ app.js
@@ -8,3 +8,17 @@ if(favouriteInstrument=='Guitar')
 else if(favouriteInstrument=='Ney')
 {document.write('<iframe width="420" height="345" src="https://www.youtube.com/embed/JFjoVCIdEgw autoplay"></iframe>')}
 
+while (favouriteInstrument !== 'Guitar' && favouriteInstrument !== 'Ney') {
+  favouriteInstrument = prompt('please only answer with Guitar or Ney');
+}
+
+var likes = prompt('how many Likes do you give our website?');
+
+if (likes > 5) {
+  Likes = 5;
+}
+
+for (var i = 0; i < likes; i++) {
+
+  document.write('<img src="https://i.pinimg.com/564x/63/3b/2e/633b2ea2a96ceaa39669a86dac0f3c01.jpg" width="50px">');
+}
